f79d8580beff23e606d613a026f340e6b3efbc6f
Cornelio <cornelioabdimash@gmail.com>
2025-11-15 10:22:39 +0700

    Update Artwork.php

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo; // <-- 1. Import this
 
 class Artwork extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Get the user (artist) that owns the artwork.
+     */
+    public function user(): BelongsTo // <-- 2. Add this entire method
+    {
+        return $this->belongsTo(User::class);
+    }
 }
