b8c15c61ee70e205a4b2200d1906fafc5c3971c4
Cornelio <cornelioabdimash@gmail.com>
2025-11-29 15:16:20 +0700

    ---- Need to Roll The Function Later --- Trust all proxies for local development
    Added TrustProxies middleware and configured the application to trust all proxies, including ngrok, by setting the proxies to '*' and specifying relevant forwarded headers. This change ensures correct handling of forwarded requests during local development.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use Illuminate\Http\Request; // <-- ADD THIS LINE
+use Illuminate\Http\Middleware\TrustProxies;
+
 // bootstrap/app.php
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -13,6 +16,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->trustProxies(
+            at: '*', // Trust all proxies for local ngrok development
+            headers: Request::HEADER_X_FORWARDED_FOR | 
+                        Request::HEADER_X_FORWARDED_HOST | 
+                        Request::HEADER_X_FORWARDED_PORT | 
+                        Request::HEADER_X_FORWARDED_PROTO | 
+                        Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+        
         
         // Add all of these aliases
         $middleware->alias([
