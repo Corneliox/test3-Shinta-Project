e457b796cfed9dc7fcec329b3276ba202fb7adf0
Cornelio <cornelioabdimash@gmail.com>
2025-12-09 12:49:33 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <title>{{ config('app.name', 'WOPANCO Admin') }}</title>
+        
         {{-- FAVICON --}}
         <link rel="icon" type="image/png" href="{{ asset('images/wopanco2.png') }}">
         
-        <title>{{ config('app.name', 'WOPANCO Admin') }}</title>
-
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
