addacb6f9c3436487e52c4cad660076b97734d47
Cornelio <cornelioabdimash@gmail.com>
2025-12-16 14:44:49 +0700

    Api input

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -60,7 +60,7 @@
     </div>
 
     {{-- YOUR TINYMCE SCRIPT --}}
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/w0mxt01iygm8l26kqy3w3okjhxfjp66y9mpfory164br98jq/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
       tinymce.init({
         selector: '#news-editor',
