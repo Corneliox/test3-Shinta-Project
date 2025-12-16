571ef89bf47c8cac37b5bc428036040bccc6f510
Cornelio <cornelioabdimash@gmail.com>
2025-12-16 14:33:08 +0700

    Update admin-sidebar.blade.php

--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -80,7 +80,7 @@
 
         {{-- News & Articles --}}
         <li>
-            <a href="{{ route('admin.news.create.blade.php') }}" 
+            <a href="{{ route('admin.news.create') }}" 
                class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ request()->routeIs('news.*') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
                 {{-- Newspaper Icon --}}
                 <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
